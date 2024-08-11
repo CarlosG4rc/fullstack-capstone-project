b2351139442cec2ae92b865c0f32beab717680c6
CarlosG4rc <carlos_solrac93@hotmail.com>
2024-08-11 13:27:42 -0600

    searching gifts

--- giftlink-backend/routes/searchRoutes.js
+++ giftlink-backend/routes/searchRoutes.js
@@ -7,14 +7,11 @@ router.get('/', async (req, res, next) => {
     try {
         // Task 1: Connect to MongoDB using connectToDatabase database. Remember to use the await keyword and store the connection in `db`
         const db =await connectToDatabase();
-
         const collection = db.collection("gifts");
-
         // Initialize the query object
         let query = {};
 
-        // Add the name filter to the query if the name parameter is not empty
-        if (req.query.name>req.query.name && req.query.name.trim() !== '') {
+        if (req.query.name && req.query.name.trim() !== '') {
             query.name = { $regex: req.query.name, $options: "i" }; // Using regex for partial match, case-insensitive
         }
 
@@ -26,13 +23,11 @@ router.get('/', async (req, res, next) => {
             query.condition = req.query.condition;
         }
         if (req.query.age_years) {
-                query.age_years = { $lte: parseInt(req.query.age_years) };
             query.age_years = { $lte: parseInt(req.query.age_years) };
         }
 
         // Task 4: Fetch filtered gifts using the find(query) method. Make sure to use await and store the result in the `gifts` constant
         const gifts = await collection.find(query).toArray();
-
         res.json(gifts);
     } catch (e) {
         next(e);
